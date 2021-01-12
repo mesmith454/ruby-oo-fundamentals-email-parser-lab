f3502af10a576cf2a0aba563328a5b0d4ccf578b
mesmith454 <mesmith454@gmail.com>
2021-01-12 14:45:17 -0800

    done

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,16 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+    attr_accessor :csv_emails, :name
+
+    def initialize (csv_emails)
+        @csv_emails = csv_emails
+    end
+
+    def parse
+       csv_emails.split(/[,?\s]/).select {|email| email != ""}.uniq
+    end
+    # got stuck and found this code from Abysmal Psycho on Github. This instructs the pase method to move through our collection of CSV email addresses, provides a collection of punctuation to filter for, select the addresses and return only unique addresses.This is my understanding of what I have found.
+end
